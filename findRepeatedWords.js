d2b79ba18a1cdb881ab8138414fa00403c178615
Tanvi Chopra <tanvi_chopra@Mckinsey.com>
2022-02-11 10:52:03 +0530

    Printed repeated words

--- findRepeatedWords.js
+++ findRepeatedWords.js
@@ -1,11 +1,10 @@
-
 const sentence = "My favourite icecream is also my sister's favourite icecream"
 function repeatedWords(string)
 {
     let words = new Set()
     let wordsArray= string.split(" ")
     len = wordsArray.length
-    for(let i=0; i<=len; i++)
+    for(let i=0; i<len; i++)
     {
         if(words.has(wordsArray[i]))
         {
